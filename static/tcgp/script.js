6980790cee67fe55a4f2f33ee91623e104485b61
Mark Rainey <97651651+raineymarkr@users.noreply.github.com>
2025-08-08 11:23:18 -0500

    Update script.js

--- static/tcgp/script.js
+++ static/tcgp/script.js
@@ -263,7 +263,7 @@ if(!loggedin){
         }
     }
 }
-if (loggedin != false) {
+if (loggedin) {
     document.getElementById('loginModal').style.display = 'none';
     tcgdex = new TCGdex('en'); 
     //saved = loadFile('http://markrainey.me/datastore/savedJson.txt');
@@ -312,6 +312,7 @@ if (loggedin != false) {
         });
     }
  }
+
 
 
 
